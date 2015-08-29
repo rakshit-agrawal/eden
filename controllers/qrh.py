3800324b2774951301e3dbb0047c0006c543793c
Rakshit Agrawal <rakshit_agrawal@outlook.com>
2015-08-29 09:51:34 -0700

    Added categories in create form. Specialized option in create controller

--- controllers/qrh.py
+++ controllers/qrh.py
@@ -15,8 +15,64 @@ if not settings.has_module(module):
 def host_volunteer(function = None, results={}):
     if function == "base":
         results['name'] = "Volunteer"
+        results['message'] = "I would like to"
+        results['options'] = []
+        results['options'].append(dict(object = A('Register a new volunteer',
+                                                  target=URL('qrh','create_entry',args=["volunteer"]),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','create_entry',args=["volunteer"])),
+                                       action="create"))
+        results['options'].append(dict(object = A('View current volunteers',
+                                                  target=URL('qrh','list_entries',args=["volunteer"]),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','list_entries',args=["volunteer"])),
+                                       action="list"))
+
     elif function == "create":
         results['name'] = "Create a new volunteer"
+        results['message'] = "I would like to"
+        results['options'] = []
+        results['options'].append(dict(object = A('Volunteer with first aid skills',
+                                                  target=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                             vars={'category':"first_aid"}),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                            vars={'category':"first_aid"})),
+                                       action="create"))
+        results['options'].append(dict(object = A('Volunteer with tech support skills',
+                                                  target=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                             vars={'category':"tech_support"}),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                            vars={'category':"tech_support"})),
+                                       action="create"))
+        results['options'].append(dict(object = A('Lead volunteer',
+                                                  target=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                             vars={'category':"lead"}),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                            vars={'category':"lead"})),
+                                       action="create"))
+        results['options'].append(dict(object = A('General volunteer',
+                                                  target=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                             vars={'category':"general"}),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','create_entry',args=["volunteer", "specialized"],
+                                                            vars={'category':"general"})),
+                                       action="create"))
+
+        """
+    firstAidLink = A('Volunteer with first aid skills', target='volunteerCategories', _class='btn btn-default btn-block btn-lg main-btn',  _role='button', _href='createAid/')
+    techSupport = A('Volunteer with tech support skills', target='volunteerSearch', _class='btn btn-default btn-block btn-lg main-btn', _role='button', _href='createTech')
+    leadVolunteer = A('Lead volunteer', target='volunteerCategories', _class='btn btn-default btn-block btn-lg main-btn',  _role='button', _href='createLeader')
+    generalVolunteer = A('General volunteer', target='volunteerSearch', _class='btn btn-default btn-block btn-lg main-btn', _role='button', _href='createGeneral')
+    """
     elif function == "list":
         results['name'] = "List all volunteers"
 
@@ -27,6 +83,21 @@ def host_volunteer(function = None, results={}):
 def host_assets(function = None, results={}):
     if function == "base":
         results['name'] = "Assets"
+        results['message'] = "I would like to"
+        results['options'] = []
+        results['options'].append(dict(object = A('Register a new asset',
+                                                  target=URL('qrh','create_entry',args=["assets"]),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','create_entry',args=["assets"])),
+                                       action="create"))
+        results['options'].append(dict(object = A('View current assets',
+                                                  target=URL('qrh','list_entries',args=["assets"]),
+                                                  _class='btn btn-default btn-block btn-lg main-btn',
+                                                  _role='button',
+                                                  _href=URL('qrh','list_entries',args=["assets"])),
+                                       action="list"))
+
     elif function == "create":
         results['name'] = "Create an asset"
     elif function == "list":
@@ -88,10 +159,20 @@ def create_entry():
     else:
         redirect(URL('qrh','index'))
 
-    try:
-        return_fields = HOSTS[host_type](function="create")
-    except KeyError, e:
-        redirect(URL('qrh','index'))
+    if len(request.args)==1:
+        try:
+            return_fields = HOSTS[host_type](function="create")
+        except KeyError, e:
+            redirect(URL('qrh','index'))
+    elif len(request.args)==2 and request.args(1)=="specialized":
+        # Then perform specialized category tasks
+        if request.vars.has_key('category'):
+            # Perform actions as per the category
+            try:
+                return_fields = HOSTS[host_type](function="create")
+            except KeyError, e:
+                redirect(URL('qrh','index'))
+
     return_fields['host_type'] = host_type.title()
     return_fields['module_list'] = [i.title() for i in HOSTS.keys()]
 
